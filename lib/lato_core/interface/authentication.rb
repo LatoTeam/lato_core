9487412c2990ff1d4380df28bd37476abf4359e2
Gregorio Galante <gregogalante@gmail.com>
2016-09-04 19:30:44 +0200

    auto-commit

--- lib/lato_core/interface/authentication.rb
+++ lib/lato_core/interface/authentication.rb
@@ -1,34 +1,30 @@
 module LatoCore
-  module Interface
-    # Insieme di funzioni che permettono di gestire l'autenticazione degli
-    # utenti che vogliono accedere al backoffice
-    module Authentication
+  module Interface::Authentication
 
-      # Funzione che controlla se l'utente ha i permessi di accedere al
-      # backoffice. Se l'utente non ha i permessi viene rimandato alla pagina di login.
-      def core_controlUser
-        redirect_to lato_core.login_path unless core_controlSession
-      end
-
-      # Funzione che ritorna l'oggetto utente loggato.
-      # * *Returns* :
-      # - user_id se la sessione dell'utente risulta valida
-      # - false se la sessione non esiste o non e' valida
-      def core_getCurrentUser
-        user = LatoCore::Superuser.find(session[:user])
-        return user if session[:user] && session[:session_code] && user
-      end
-
-      # Funzione che controlla se i permessi dell'utente sono superiori al
-      # livello ricevuto come parametro
-      # * *Returns* :
-      # - true se la sessione dell'utente ha permessi maggiori o uguali del parametro ricevuto
-      # - false se la sessione non esiste o non ha i permessi necessari
-      def core_controlPermission(permission)
-        user = core_getCurrentUser
-        user && user.permission >= permission
-      end
-
+    # Check if user is logged. Redirect to login path if user is not logged.
+    def core_controlUser
+      redirect_to lato_core.login_path unless core_controlSession
     end
+
+    # Return user logged activerecord object.
+    # * *Returns* :
+    # - user if session is correct
+    # - nil if session not exist
+    def core_getCurrentUser
+      user = LatoCore::Superuser.find(session[:user])
+      return user if session[:user] && session[:session_code] && user
+    end
+
+    # Check if user permission is equal or greater than param
+    # * *Params* :
+    # - permission: integer value between 1 to 10
+    # * *Returns* :
+    # - true if user permission is equal or greater than param
+    # - false if user permission is not equal or greater than param
+    def core_controlPermission(permission)
+      user = core_getCurrentUser
+      user && user.permission >= permission
+    end
+
   end
 end
--- lib/lato_core/interface/cache.rb
+++ lib/lato_core/interface/cache.rb
@@ -1,28 +1,23 @@
 module LatoCore
-  module Interface
-    # Insieme di funzioni che gestiscono la cache dei moduli sulla
-    # applicazione principale.
-    module Cache
+  module Interface::Cache
 
-      # Funzione che ritorna il percorso alla directory di cache usata dai
-      # moduli lato per memorizzare informazioni sull'applicazione principale.
-      # La directory dell'app principale usata come cache si trova nel percorso
-      # #{Rails.root}/config/lato
-      def core_getCacheDirectory
-        # creo le directory per gli assets se non esistono
-        dirname = "#{Rails.root}/app/assets/images/lato"
-        FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
-        dirname = "#{Rails.root}/app/assets/stylesheets/lato"
-        FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
-        dirname = "#{Rails.root}/app/assets/javascripts/lato"
-        FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
-        # creo la directory di cache se non esiste
-        dirname = "#{Rails.root}/config/lato"
-        FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
-        # ritorno la directory di cache
-        return dirname
-      end
-
+    # Funzione che ritorna il percorso alla directory di cache usata dai
+    # moduli lato per memorizzare informazioni sull'applicazione principale.
+    # La directory dell'app principale usata come cache si trova nel percorso
+    # #{Rails.root}/config/lato
+    def core_getCacheDirectory
+      # creo le directory per gli assets se non esistono
+      dirname = "#{Rails.root}/app/assets/images/lato"
+      FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
+      dirname = "#{Rails.root}/app/assets/stylesheets/lato"
+      FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
+      dirname = "#{Rails.root}/app/assets/javascripts/lato"
+      FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
+      # creo la directory di cache se non esiste
+      dirname = "#{Rails.root}/config/lato"
+      FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
+      # ritorno la directory di cache
+      return dirname
     end
 
   end
